0dc7f1d0913ee0ee23b86cc1e17d5bd58845e781
Jeon, Minseo <minseo4565@snu.ac.kr>
2025-05-15 00:51:32 +0900

    feat: GameManager.prefab 에 SceneLoadManager Object 추가
    - SceneLoadManager 오브젝트를 GameManager 오브젝트에 추가하여 실제 게임에 적용되게 했습니다.
    - FadeIn/FadeOut 로직을 조금 수정했습니다. (CanvasGroup 사용 대신, Image 의 알파값만 사용하도록...)

--- Assets/Scripts/GameManager/SceneLoadManager.cs
+++ Assets/Scripts/GameManager/SceneLoadManager.cs
@@ -8,40 +8,33 @@ public class SceneLoadManager : MonoBehaviour
 {
     public Image fadeImage;
     public float fadeSpeed = 1.5f;
-    public CanvasGroup uiCanvasGroup;
     
     private IEnumerator FadeOut()
     {
         float alpha = 0;
         fadeImage.color = new Color(0, 0, 0, 0);
-        uiCanvasGroup.alpha = 1; // Ensure UI is fully visible at start
 
         while (alpha < 1)
         {
             alpha += Time.deltaTime * fadeSpeed;
             fadeImage.color = new Color(0, 0, 0, alpha);
-            uiCanvasGroup.alpha = 1 - alpha; // Fade out UI
             yield return null;
         }
-
-        uiCanvasGroup.alpha = 0; // Ensure UI is fully transparent at end
+        
     }
 
     private IEnumerator FadeIn()
     {
         float alpha = 1;
         fadeImage.color = new Color(0, 0, 0, 1);
-        uiCanvasGroup.alpha = 0; // Ensure UI is hidden at start
 
         while (alpha > 0)
         {
             alpha -= Time.deltaTime * fadeSpeed;
             fadeImage.color = new Color(0, 0, 0, alpha);
-            uiCanvasGroup.alpha = 1 - alpha; // Fade in UI
             yield return null;
         }
-
-        uiCanvasGroup.alpha = 1; // Ensure UI is fully visible at end
+        
     }
 
     public IEnumerator FadeAndLoadScene(string sceneName)
